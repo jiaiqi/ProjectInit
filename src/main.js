833d029d423fccccf6775745d5eb137a9eea413e
jiaqi <2468260248@qq.com>
2019-09-19 15:43:11 +0800

    增加axios、以及axios请求和响应拦截函数

--- src/main.js
+++ src/main.js
@@ -2,8 +2,34 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios';
+import VueInit from '@/assets/dev_config/vue_init';
+import VueUtil from '@/assets/dev_config/vue_util';
+
 
 Vue.config.productionTip = false
+
+VueInit();
+VueUtil();
+
+Vue.prototype.axios = axios; // 挂载axios到vue.proto对象上原型属性
+
+axios.interceptors.request.use(config => {
+//在发送请求之前做些什么
+return config;
+},error => {
+// Do something with request error
+return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+// Do something before response is sent
+return response;
+},error => {
+// Do something with response error
+return Promise.reject(error);
+});
+
 
 new Vue({
   router,
